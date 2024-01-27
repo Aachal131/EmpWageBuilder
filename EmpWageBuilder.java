ba135fc9812ca5c2ca111a4ed4276e0ac599f06a
Aachal131 <83298583+Aachal131@users.noreply.github.com>
2024-01-27 21:47:03 +0530

    Add files via upload

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,34 @@
 package com.lerning;
 
 public class EmpWageBuilder {
-    public static void main (String args[]){
-        System.out.println("Welcome to Lerning");
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
+
+    public static void main(String args[]) {
+
+        int empHrs = 0;
+        int totalEmpHrs= 0;
+        int totalWorkingdays =0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingdays <NUM_OF_WORKING_DAYS){
+            totalWorkingdays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day#:"+ totalWorkingdays +"Emp Hr:" +empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage:" + totalEmpWage);
     }
 }
